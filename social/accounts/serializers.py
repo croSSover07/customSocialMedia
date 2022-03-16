537dbbabd07d227e03df1d5f7145040e0e5e6c29
Ilya M <work.ilya.merkotun@gmail.com>
2022-03-16 22:35:08 +0200

    - hide password in Account

--- social/accounts/serializers.py
+++ social/accounts/serializers.py
@@ -6,4 +6,5 @@ from .models import Account
 class AccountSerializer(ModelSerializer):
     class Meta:
         model = Account
+        extra_kwargs = {'password': {'write_only': True}}
         fields = '__all__'
