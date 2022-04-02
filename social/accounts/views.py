70b48831495e0b6742a84a069b970b6ae4620fcb
Ilya M <work.ilya.merkotun@gmail.com>
2022-04-02 19:06:01 +0300

    - add global permissions  - remove auth perm from 'register' request

--- social/accounts/views.py
+++ social/accounts/views.py
@@ -1,3 +1,6 @@
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
 from .models import Account
 from rest_framework.viewsets import ModelViewSet
 
@@ -9,3 +12,11 @@ from .serializers import AccountSerializer
 class AccountViewSet(ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method == 'POST':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
