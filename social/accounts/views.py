19bbee212bc8ffa26b090211628c862c9f071700
Ilya M <work.ilya.merkotun@gmail.com>
2022-04-16 21:24:23 +0300

    - send email when create acc  by selery

--- social/accounts/views.py
+++ social/accounts/views.py
@@ -7,6 +7,7 @@ from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 from .serializers import AccountSerializer
+from .tasks import send_information_email
 
 
 class AccountViewSet(ModelViewSet):
@@ -20,3 +21,9 @@ class AccountViewSet(ModelViewSet):
         else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        send_information_email.delay(self.kwargs.get('email'))
+
+
--- social/social/settings.py
+++ social/social/settings.py
@@ -59,6 +59,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REDIS_HOST = 'localhost'
+REDIS_PORT = '6379'
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+
 ROOT_URLCONF = 'social.urls'
 
 TEMPLATES = [
